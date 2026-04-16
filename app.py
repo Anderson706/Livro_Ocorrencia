22820b35ed8f35fc531dbff1d3b33d631bbfc82c
Deivid Leite <deivid123.leite@gmail.com>
2026-04-16 04:28:11 -0300

    fechamento

--- app.py
+++ app.py
@@ -2094,4 +2094,4 @@ if __name__ == "__main__":
     t.start()
 
     webview.create_window('Livro de Ocorrências DHL Security', 'http://127.0.0.1:5000', width=1280, height=850, resizable=True)
-    webview.start(private_mode=True, debug=True)
+    webview.start(private_mode=True)
